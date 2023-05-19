af3a8dcdbf71998979091d24471af64fbebb7fa5
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2023-05-19 11:11:37 -0300

    Adiciona domínio e interface para Aluno

---
+++ backend/core/interfaces/repositories.py
@@ -0,0 +1,29 @@
+"""Repository interface for domain layer.
+
+- A Interface define as operações que o domínio precisa.
+- A implementação das operações é feita fora, no adaptador.
+- Domínio está desacoplado de qualquer implementação específica
+"""
+
+from abc import ABC, abstractmethod
+from backend.core.domain.models import Aluno
+
+
+class AlunoRepository(ABC):
+    """Aluno repository interface for domain layer. It defines the methods
+    for saving, retrieving, and deleting Aluno objects."""
+
+    @abstractmethod
+    def save(self, aluno: Aluno) -> Aluno:
+        """Saves an Aluno object to the repository."""
+        pass
+
+    @abstractmethod
+    def get_by_id(self, aluno_id: int) -> Aluno:
+        """Retrieves an Aluno object from the repository by its ID."""
+        pass
+
+    @abstractmethod
+    def delete(self, aluno: Aluno) -> None:
+        """Deletes an Aluno object from the repository."""
+        pass
