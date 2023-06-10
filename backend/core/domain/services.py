dfdf901ca6402786b391e87d8fc81d6f8a51071f
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2023-06-10 15:47:07 -0300

    Adiciona API no backend para pesquisar de forma paginada

--- backend/core/domain/services.py
+++ backend/core/domain/services.py
@@ -1,163 +1,223 @@
-"""Domain layer services module. It follows hexagonal architecture principles.
+"""Database repositories module.
 
-- O serviço está desacoplado de qualquer implementação específica,
-  tal como DB ou ORM
+- Implementa as operações necessárias para o domínio salvar os dados
+- Implementa a interface para salvar e recuperar dados do DB
+- Aqui, pode ser usado qualquer tecnologia
 """
 
-from backend.core.domain.models import Aluno, Professor
-from backend.core.interfaces.repositories import (
-    AlunoRepository
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import column
+
+from backend.core.domain.models import (
+    Professor, Aluno
+)
+from backend.core.interfaces.repositories import(
+    ProfessorRepository, AlunoRepository
 )
 
+from backend.adapters.database.database import ProfessorORM, AlunoORM
 
-class ProfessorService:
-    """Professor service class for domain layer. It provides methods for
-    verifying professor login credentials. It takes a professor_repository
-    object as a dependency."""
+class ProfessorRepositoryPostgres(ProfessorRepository):
+    """Professor repository class for PostgreSQL implementation."""
 
-    def __init__(self, professor_repository):
-        """Initializes the service with a ProfessorRepository object."""
-        self.professor_repository = professor_repository
+    def __init__(self, db: Session):
+        """Initializes the repository with a database session."""
+        self.database = db
 
-    def login(self, email, password):
-        """Verifies professor login credentials."""
-        login = self.professor_repository.verify_login(email, password)
-        return login
 
-    def create_professor(self, name, email, password):
-        """Creates a new Professor object and saves it to the repository.
-        When creating a new Professor object, the ID does not need to be
-        provided, as it is generated automatically by the repository."""
-        professor = Professor(
-            id=None,
-            name=name,
-            email=email,
-            password=password
-        )
-        self.professor_repository.save(professor)
-        if professor is not None:
-            return professor
-        return False
+    def verify_login(self, professor_email: str, professor_password: str) -> bool | str:
+        """Verifies professor login credentials. It queries the database for
+        a professor with the given email and checks if the password matches.\\
+        Args:
+            email (str): Professor's email.
+            password (str): Professor's password.
+        Returns:
+            bool | str: True if the credentials are valid. False if the
+                credentials are invalid. Otherwise, returns an error message.
+        """
+        try:
+            professor_object = self.database.query(ProfessorORM).filter_by(email=professor_email).first()
+        except SQLAlchemyError as exception:
+            error_message = f"An error occurred: {exception}"
+            print(error_message)
+            return error_message
+        if professor_object is None:
+            return False
 
-    def update_professor(self, professor_id, name=None, email=None,
-                        password=None):
-        """Updates an existing Professor object and saves it to the repository."""
-        professor = self.professor_repository.get_by_id(professor_id)
-        if professor_id is not None:
-            professor.id = professor_id
-        if name is not None:
-            professor.name = name
-        if email is not None:
-            professor.email = email
-        if password is not None:
-            professor.password = password
-        self.professor_repository.save(professor)
-        if professor is not None:
-            return professor
-        return False
+        return professor_object.password == professor_password
 
-    def remove_professor(self, professor_id):
-        """Removes an existing Professor object from the repository."""
-        professor = self.professor_repository.get_by_id(professor_id)
-        self.professor_repository.delete(professor)
 
-    def get_professor_by_name(self, professor_name):
-        """Retrieves an existing Professor object from the repository."""
-        professor = self.professor_repository.get_by_name(professor_name)
+    def save(self, professor: Professor) -> Professor | str:
+        """Saves a Professor object to the database. If the save fails,
+        an exception is raised and the error message is returned.\\
+        Args:
+            professor (Professor): Professor object to be saved.
+        Returns:
+            Professor | str: Professor object if the save is successful.
+                Otherwise, returns an error message.
+        """
+        try:
+            professor_orm = ProfessorORM.from_professor(professor)
+            self.database.add(professor_orm)
+            self.database.commit()
+            self.database.refresh(professor_orm)
+        except SQLAlchemyError as exception:
+            error_message = f"An error occurred: {exception}"
+            print(error_message) # TODO: Remove later
+            return error_message
         return professor
 
 
-class AlunoService:
-    """Aluno service class for domain layer. It provides methods for
-    creating, updating, and removing Aluno objects. It takes an
-    aluno_repository object as a dependency."""
-
-    def __init__(self, aluno_repository: AlunoRepository):
-        """Initializes the service with an AlunoRepository object."""
-        self.aluno_repository = aluno_repository
-
-    def create_aluno(self, name, born_date, address, tutor_name,
-                    tutor_phone, class_shift):
-        """Creates a new Aluno object and saves it to the repository.
-        When creating a new Aluno object, the ID does not need to be
-        specified, since it is generated automatically by the database.
-        """
-        aluno = Aluno(
-            id=None,
-            name=name,
-            born_date=born_date,
-            address=address,
-            tutor_name=tutor_name,
-            tutor_phone=tutor_phone,
-            class_shift=class_shift
-        )
-        aluno = self.aluno_repository.save(aluno)
-        if isinstance(aluno, str): # if aluno is not found
-            return aluno
-        return aluno
-
-
-    def update_aluno(self, aluno_id, name=None, born_date=None,
-                        address=None, tutor_name=None, tutor_phone=None,
-                        class_shift=None):
-        """Updates an existing Aluno object and saves it to the repository."""
-        aluno = self.aluno_repository.get_by_id(aluno_id)
-        if isinstance(aluno, str): # if aluno is not found
-            return aluno
-
-        if name is not None:
-            aluno.name = name
-        if born_date is not None:
-            aluno.born_date = born_date
-        if address is not None:
-            aluno.address = address
-        if tutor_name is not None:
-            aluno.tutor_name = tutor_name
-        if tutor_phone is not None:
-            aluno.tutor_phone = tutor_phone
-        if class_shift is not None:
-            aluno.class_shift = class_shift
-        self.aluno_repository.save(aluno)
-        return aluno
-
-
-    def remove_aluno(self, aluno_id):
-        """Removes an existing Aluno object from the repository."""
-        aluno = self.aluno_repository.get_by_id(aluno_id)
-        message = self.aluno_repository.delete(aluno)
-        return message
-
-
-    def get_alunos_by_name(self, aluno_name: str) -> dict:
-        """Retrieves one or more existing Aluno object from the repository.
-        Returns a dictionary of alunos, where every aluno object is a
-        dictionary itself.\\
+    def delete(self, professor: Professor) -> str:
+        """Deletes a Professor object from the database.  If the deletion fails,
+        an exception is raised and the error message is returned.\\
         Args:
-            aluno_name (str): The name of the aluno to be retrieved.
+            professor (Professor): Professor object to be deleted.
         Returns:
-            alunos_dict (dict): A dictionary of alunos.
+            str: Error message if the deletion fails. Otherwise,
+                returns a success message.
         """
-        alunos = self.aluno_repository.get_by_name(aluno_name)
+        try:
+            self.database.delete(professor)
+            self.database.commit()
+        except SQLAlchemyError as exception:
+            error_message = f"An error occurred: {exception}"
+            print(error_message) # TODO: Remove later
+            return error_message
+        return "Removed successfully"
 
-        # get all alunos and change it to a dictionary of alunos
-        alunos_dict = {'Aluno': []} # {'Aluno': [{'id': 1, name: 'joao',...}, aluno2, ...]}
-        for aluno in alunos:
-            alunos_dict['Aluno'].append(aluno.__dict__)
 
-        return alunos_dict
-
-    def get_all_alunos(self) -> dict:
-        """Retrieves all existing Aluno objects from the repository.
-        Returns a dictionary of alunos, where every aluno object is a
-        dictionary itself.\\
+    def get_by_id(self, professor_id: int) -> Professor | str:
+        """Retrieves a Professor object from the database by its ID. If the
+        retrieval fails, an error message is returned.\\
+        Args:
+            professor_id (int): Professor's ID.
         Returns:
-            alunos_dict (dict): A dictionary of alunos.
+            Professor | str: Professor object if the retrieval is successful.
+                Otherwise, returns an error message.
         """
-        alunos = self.aluno_repository.get_all_alunos()
+        professor = self.database.query(Professor).filter_by(id=professor_id).first()
+        if professor is None:
+            return f"Professor with ID {professor_id} not found"
+        return professor
 
-        # get all alunos and change it to a dictionary of alunos
-        alunos_dict = {'Aluno': []}
-        for aluno in alunos:
-            alunos_dict['Aluno'].append(aluno.__dict__)
 
-        return alunos_dict
+    def get_by_name(self, professor_name: str) -> list[Professor]:
+        """Retrieves one or more Professor object from the database by its name.\\
+        Args:
+            professor_name (str): Professor name.
+        Returns:
+            list[Professor]: List of Professor objects.
+        """
+        professors = self.database.query(Professor).filter(column('name') == professor_name).all()
+        if professors is None:
+            return []
+        return professors
+
+
+class AlunoRepositoryPostgres(AlunoRepository):
+    """Aluno repository class for PostgreSQL implementation."""
+
+    def __init__(self, db: Session):
+        """Initializes the repository with a database session."""
+        self.database = db
+
+
+    def save(self, aluno: Aluno) -> Aluno | str:
+        """Saves an Aluno object to the database. If the save fails,
+        an exception is raised and the error message is returned.\\
+        Args:
+            aluno (Aluno): Aluno object to be saved.
+        Returns:
+            Aluno | str: Aluno object if the save is successful. Otherwise,
+                returns an error message.
+        """
+        try:
+            aluno_orm = AlunoORM.from_aluno(aluno)
+            self.database.add(aluno_orm)
+            self.database.commit()
+            self.database.refresh(aluno_orm)
+        except SQLAlchemyError as exception:
+            error_message = f"An error occurred: {exception}"
+            print(error_message) # TODO: Remove later
+            return error_message
+        return aluno
+
+
+    def get_by_id(self, aluno_id: int) -> Aluno | str:
+        """Retrieves an Aluno object from the database by its ID. If the
+        retrieval fails, an error message is returned.\\
+        Args:
+            aluno_id (int): Aluno's ID.
+        Returns:
+            Aluno | str: Aluno object if the retrieval is successful. Otherwise,
+                returns an error message.
+        """
+        aluno = self.database.query(AlunoORM).filter_by(id=aluno_id).first()
+        if aluno is None:
+            return f"Aluno with ID {aluno_id} not found"
+        return aluno
+
+
+    def delete(self, aluno: Aluno) -> str:
+        """Deletes an Aluno object from the database. If the deletion fails,
+        an exception is raised and the error message is returned.\\
+        Args:
+            aluno (Aluno): Aluno object to be deleted.
+        Returns:
+            str: Error message if the deletion fails. Otherwise,
+                returns a success message.
+        """
+        try:
+            self.database.delete(aluno)
+            self.database.commit()
+        except SQLAlchemyError as exception:
+            error_message = f"An error occurred: {exception}"
+            print(error_message) # TODO: Remove later
+            return error_message
+        return "Removed successfully"
+
+
+    def get_by_name(self, aluno_name: str) -> list[Aluno]:
+        """Retrieves one or more Aluno object from the database by its name.\\
+        Args:
+            aluno_name (str): Aluno name.
+        Returns:
+            list[Aluno]: List of Aluno objects.
+        """
+        alunos = self.database.query(AlunoORM).filter(column('name') == aluno_name).all()
+        alunos = [AlunoORM.to_aluno(aluno) for aluno in alunos]
+        if alunos is None:
+            return []
+        return alunos
+
+
+    def get_all_alunos(self) -> list[Aluno]:
+        """Retrieves all Aluno objects from the database. If the retrieval
+        fails, an empty list is returned.\\
+        Returns:
+            list[Aluno]: List of Aluno objects.
+        """
+        alunos_orm = self.database.query(AlunoORM).all()
+        alunos = [AlunoORM.to_aluno(aluno) for aluno in alunos_orm]
+        if alunos is None:
+            return []
+        return alunos
+
+
+    def get_alunos_paginated(self, offset, limit, name_like) -> list[Aluno]:
+        """Retrieves all Aluno objects from the database. If the retrieval
+        fails, an empty list is returned.\\
+        Returns:
+            list[Aluno]: List of Aluno objects.
+        """
+        alunos_orm = self.database.query(
+                         AlunoORM).filter(
+                         AlunoORM.name.like(f'%{name_like}%')
+                         ).offset(offset).limit(limit).all()
+
+        alunos = [AlunoORM.to_aluno(aluno) for aluno in alunos_orm]
+        if alunos is None:
+            return []
+        return alunos
